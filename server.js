c750990c11946abe91b892022dc51802a044455d
mirsahib <mirsahib24@gmail.com>
2021-01-07 11:33:43 +0600

    :bulb: Add comment

--- server.js
+++ server.js
@@ -1,33 +1,60 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const app = express();
+//declare server port
 const port = process.env.PORT || 5000;
 
+//handle environment variable (.env file)
+require("dotenv").config();
+
+//express setup
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//bodyparser configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
-
-if (process.env.NODE_ENV === 'production') {
+//config server to handle front-end in production env
+if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
+//database setup
+mongoose.connect(
+  process.env.DB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) console.log(err);
+    console.log("Database Connected");
+  }
+);
+
+// router
+app.get("/api/hello", (req, res) => {
+  res.send({ express: "Hello From Express" });
+});
+
+app.post("/api/world", (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`
+  );
+});
+
+// server listener
 app.listen(port, () => console.log(`Listening on port ${port}`));
