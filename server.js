8a53c22e78001a85305842c4a34a2c3a3043fb86
mirsahib <mirsahib24@gmail.com>
2021-01-07 12:10:11 +0600

    :hammer: Add user authentication routes

--- server.js
+++ server.js
@@ -45,16 +45,7 @@ mongoose.connect(
 );
 
 // router
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
-});
-
-app.post("/api/world", (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
+app.use("/users", require("./routes/user.route"));
 
 // server listener
 app.listen(port, () => console.log(`Listening on port ${port}`));
