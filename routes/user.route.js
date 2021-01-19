7a48d486d9a8bd1917c56a24517aff0b28d1c11b
mirsahib <mirsahib24@gmail.com>
2021-01-20 00:18:57 +0600

    :hammer: Change /auth route to GET

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,7 +4,7 @@ const userCtrl = require("../controller/user.controller");
 const authCtrl = require("../middleware/auth");
 
 router.route("/").get(authCtrl, userCtrl.getUserId); // default route to get user id
-router.route("/auth").post(userCtrl.isTokenValid); // route to check user token validation
+router.route("/auth").get(userCtrl.isTokenValid); // route to check user token validation
 router.route("/register").post(userCtrl.userRegistration); // route to create new user
 router.route("/login").post(userCtrl.userLogin); // route to give access to existing user
 
